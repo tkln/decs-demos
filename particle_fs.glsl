5fc1841963a6788803c9f80d209c39da5679dd56
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-01-26 20:21:44 +0200

    particle: Make the particle rendering scale independent

--- particle_fs.glsl
+++ particle_fs.glsl
@@ -10,11 +10,17 @@ void main() {
     vec2 c = vec2((2.0 / 720.0) * gl_FragCoord.x - (1280.0 / 720.0),
                   (2.0 / 720.0) * gl_FragCoord.y - 1.0);
 
+    const float f = 0.02;
     float d = distance(center_pos.xy, c.xy);
+    float a = 1.0f;
 
     if (d > particle_scale)
         discard;
 
-    color = vec4(particle_color,
-                 pow((particle_scale / (d * 6)), 2) * (1.0 - d * 20.0));
+    if (d > (particle_scale - f))  {
+        float fd = (d - (particle_scale - f)) / f;
+        a *= 1.0 - pow(fd, 2.0);
+    }
+
+    color = vec4(particle_color, a);
 }
