4320d101b4209265cfcc24566ae6c22123c6bc28
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-01-27 13:19:31 +0200

    particle: Make the shader scale independent

--- particle_fs.glsl
+++ particle_fs.glsl
@@ -10,16 +10,17 @@ void main() {
     vec2 c = vec2((2.0 / 720.0) * gl_FragCoord.x - (1280.0 / 720.0),
                   (2.0 / 720.0) * gl_FragCoord.y - 1.0);
 
-    const float f = 0.02;
+    const float falloff = 0.05;
+    const float hardness = 5.5;
     float d = distance(center_pos.xy, c.xy);
     float a = 1.0f;
 
     if (d > particle_scale)
         discard;
 
-    if (d > (particle_scale - f))  {
-        float fd = (d - (particle_scale - f)) / f;
-        a *= 1.0 - pow(fd, 2.0);
+    if (d > (particle_scale - falloff))  {
+        float fd = (d - (particle_scale - falloff)) / falloff;
+        a *= 1.0 - pow(fd * hardness, 3.0);
     }
 
     color = vec4(particle_color, a);
